e430244e365c45ddc989bc17a4486817a5cc1994
Daniel Perez <ing.dany@gmail.com>
2020-07-22 17:49:27 -0700

    DetailVideo /get and /put to update specific record

--- video/urls.py
+++ video/urls.py
@@ -1,6 +1,7 @@
-from .views import ListVideo
-from django.urls import path
+from .views import ListVideo, DetailVideo
+from django.urls import path, re_path
 
 urlpatterns = [
-    path('videos',ListVideo.as_view(),name='list-video')
+    path('videos',ListVideo.as_view(), name='list-video'),
+    re_path(r'^videos/(?P<pk>[0-9]+)$', DetailVideo.as_view(), name='detail-video')
 ]
--- video/views.py
+++ video/views.py
@@ -3,6 +3,7 @@ from .models import Video
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import VideoSerializer
+from django.http import Http404
 
 # Create your views here.
 class ListVideo(APIView):
@@ -17,3 +18,23 @@ class ListVideo(APIView):
             video_json.save()
             return Response(video_json.data, status= 201)
         return Response(video_json.errors, status= 400)
+
+class DetailVideo(APIView):
+    def get(self, request, pk):
+        try:
+            video = Video.objects.get(pk=pk)
+            video_json = VideoSerializer(video)
+            return Response(video_json.data)
+        except Video.DoesNotExist:
+            raise Http404
+    
+    def put(self, request, pk):
+        try:
+            video = Video.objects.get(pk=pk)
+            video_json = VideoSerializer(video, data=request.data)
+            if video_json.is_valid():
+                video_json.save()
+                return Response(video_json.data, status=201)
+            return Response(video_json.errors, status= 400)
+        except Video.DoesNotExist:
+            raise Http404
